479d64ea802415c29c0d2e9efeb3db4fbe68da88
Evan Goss <evan.goss@gmail.com>
2016-08-30 14:07:16 -0700

    Serve static files

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/client'));
 
 var config = {
   port: 3000
 };
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
-});
+// app.get('/', function(req, res) {
+//   // console.log('req:', req);
+//   // console.log('res:', res);
+//   res.send('Hello world!');
+// });
 
 app.listen(config.port, function() {
   console.log('Express server listening on port', config.port);
